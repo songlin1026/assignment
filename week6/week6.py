ab04dba310a91820a34c9e7897ec76a42f211735
SongLin <songlin1026@gmail.com>
2021-03-31 22:40:08 +0800

    week6-change

--- week6/week6.py
+++ week6/week6.py
@@ -26,59 +26,54 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route("/")
 def hmoepage():
-    if session.get('username')=='member':
+    if session.get('username')==False:
+        return render_template("homePage.html")
+    else:
         return redirect("/member")
-    return render_template("homePage.html")
 
 
 @app.route("/signup",methods=["POST"])
 def signup():
-    cursor.execute("select * from week6.user ")
-    data=cursor.fetchall()
     signupName=request.form["signup_name"]
     signupAccount=request.form["signup_account"]
     signupPassword=request.form["signup_password"]
-    x=0
-    while x<len(data):
-        username_arr=data[x][2]
-        password_arr=data[x][3]
-        if signupAccount==username_arr:
-            return render_template("error.html",name="此帳號已經被註冊")
-        else:
-            x+=1
-    cursor.execute("insert into user(name,username,password) values (%s,%s,%s) ",[signupName,signupAccount,signupPassword]) 
-    connection.commit()
-    return redirect("/")
+    cursor.execute("select * from week6.user where username=%s",[signupAccount])
+    data=cursor.fetchone()
+    if data == None :
+        cursor.execute("insert into user(name,username,password) values (%s,%s,%s) ",[signupName,signupAccount,signupPassword]) 
+        connection.commit()
+        return redirect("/")
+    else:
+        return redirect(url_for("error",message="帳號已經被註冊過了"))
+
 
 
 @app.route("/signin",methods=["POST"])
 def signin():
-    cursor.execute("select * from week6.user ")
-    data=cursor.fetchall()
-    account=request.form["signin_account"]
-    password=request.form["signin_password"]
-    n=0
-    while n<len(data):
-        username_arr=data[n][2]
-        password_arr=data[n][3]
-        name=data[n][1]
-        if account==username_arr and password==password_arr:
-            session['username']= data[n][1]
-            return render_template("member.html",name=name)
+    signinAccount=request.form["signin_account"]
+    signinPassword=request.form["signin_password"]
+    cursor.execute("select * from week6.user where username=%s",[signinAccount])
+    data=cursor.fetchone()
+    if data != None :
+        if data[3]== signinPassword :
+            session['username'] = data[1]
+            return redirect(url_for("member"))
         else:
-            n+=1
-    return redirect(url_for("error",name="帳號或密碼輸入錯誤"))
+            return redirect(url_for("error",message="帳號或密碼錯誤"))
+    else:
+        return redirect(url_for("error",message="帳號或密碼錯誤"))
+
 
 
 @app.route("/member")
 def member():
-    z=0
-    while z<len(data):
-        if session.get('username')==data[z][1]:
-            return render_template("member.html",name=data[z][1])
-        else:
-            z+=1
-    return redirect("/")
+    if session.get('username') == False :
+        return redirect("/")
+    else:
+        member=session.get('username')
+        cursor.execute("select * from week6.user where name=%s",[member])
+        data=cursor.fetchone()
+        return render_template("member.html",name=data[1])
 
 
 @app.route("/signout")
@@ -87,14 +82,15 @@ def signout():
     return render_template("signout.html")
 
 
-@app.route("/error")
+@app.route("/error/")
 def error():
-    y=0
-    while y<len(data):
-        if session.get('username')==data[y][1]:
-            return redirect("/member")
-        else:
-            y+=1
-    return render_template("error.html",name="帳號或密碼輸入錯誤")
+    if session.get('username') != False :
+        member=session.get('username')
+        cursor.execute("select * from week6.user where name=%s",[member])
+        data=cursor.fetchone()
+        return redirect(url_for("member")) 
+    else:
+        content=request.args.get('message')
+        return render_template("error.html",message=content)
 
 app.run(port=3000)
